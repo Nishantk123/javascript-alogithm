2f6974240f73b850efec0f0f8b1163aa2762ea6e
Nishant kashyap <sannukashyap24@gmail.com>
2018-10-13 23:23:27 +0530

    Update exercises/reversestring/index.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+  str.split().reduce((rev, char)=>char + rev , '');
+}
+
+// function reverse(str) {
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//     let reversed = '';
+//      for(let character of str){
+  
+//          reversed = character + reversed;
+//      }
+//      return reversed;
+//  }
 
 module.exports = reverse;
