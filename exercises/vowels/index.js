91c0e192cde75536edf291a6da642c0da73b08b4
Nishant kashyap <sannukashyap24@gmail.com>
2018-10-23 23:16:14 +0530

    commit

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let count = 0;
+    const checker = "aeiou";
+    for(let char of str.toLoweCase()){
+    if(checker.includes(char)){
+        counter++;
+    }
+    }
+    return count;
+}
 
 module.exports = vowels;
